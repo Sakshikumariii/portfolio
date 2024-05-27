f66a3c3bfca38f76012cfd171dfd538af1136217
Sakshi Kumari <sakshismp2003@gmail.com>
2024-05-27 16:37:19 +0530

    first

--- script.js
+++ script.js
@@ -13,43 +13,37 @@ function showTab(tabId) {
 }
 
 // contact form
-   
-   function validateForm() {
-       var emailInput = document.getElementById("email");
-       var emailError = document.getElementById("emailError");
-       var emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
- 
-       if (!emailPattern.test(emailInput.value)) {
-           emailError.style.display = "block";
-           return false;
-       } else {
-           emailError.style.display = "none";
-           submitForm();
-           return false; // Prevent default form submission
-       }
-   }
- 
-   function submitForm() {
-       const scriptURL = 'https://script.google.com/macros/s/AKfycbwNMZRg50D_F4AILTG_ArOwdnlOolPjIYxQxB-OK6Q8dmunReSXKd89EGtu9t1eNTVzrA/exec';
-       const form = document.forms['submit-to-google-sheet'];
-       const msgElement = document.getElementById('msg');
- 
-       var xhr = new XMLHttpRequest();
-       xhr.open('POST', scriptURL);
-       xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-       xhr.onload = function() {
-           if (xhr.status === 200) {
-               msgElement.textContent = "Message sent successfully.";
-               form.reset();
-           } else {
-               console.error('Request failed. Status:', xhr.status);
-               msgElement.textContent = "An error occurred. Please try again later.";
-           }
-       };
-       xhr.send(new FormData(form));
-   }
+function validateForm() {
+   var emailInput = document.getElementById("email");
+   var emailError = document.getElementById("emailError");
+   var emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
+   if (!emailPattern.test(emailInput.value)) {
+      emailError.style.display = "block";
+      return false;
+   } else {
+      emailError.style.display = "none";
+      submitForm();
+      return false; // Prevent default form submission
+   }
+}
 
+function submitForm() {
+   const scriptURL = 'https://script.google.com/macros/s/AKfycbwNMZRg50D_F4AILTG_ArOwdnlOolPjIYxQxB-OK6Q8dmunReSXKd89EGtu9t1eNTVzrA/exec';
+   const form = document.forms['submit-to-google-sheet'];
+
+   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+         if (response.ok) {
+            alert("Message sent successfully.");
+            form.reset();
+           
+         } else {
+            throw new Error('Network response was not ok.');
+         }
+      })
+      
+}
 
 // footer getCurrent Year
 function getCurrentYear() {
